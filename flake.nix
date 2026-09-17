4004a1237ac362acdf7291c6763ec258e5f9ac27
alexmatiasas <alejandromatiasastorga@gmail.com>
2026-09-17 16:54:54 -0600

    refactor(flake): reformat and structure outputs

--- flake.nix
+++ flake.nix
@@ -6,15 +6,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    formatter.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.nixfmt-rfc-style;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    {
+      formatter.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.nixfmt-rfc-style;
 
-    nixosConfigurations.rpi-server = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
+      nixosConfigurations.rpi-server = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
 
-      modules = [
-        ./hosts/rpi-server
-      ];
+        modules = [
+          ./hosts/rpi-server
+        ];
+      };
     };
-  };
 }
