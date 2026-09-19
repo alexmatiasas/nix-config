0c1969ac419040ba3a8d7bd9655b2ec07354c32d
alexmatiasas <alejandromatiasastorga@gmail.com>
2026-09-18 18:10:54 -0600

    feat(desktop): add GNOME configuration
    - Added GNOME configuration to the desktop module
    - Enabled the desktop environment GNOME and its display manager
    - Installed a minimal set of GNOME applications to reduce bloatware

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -5,5 +5,6 @@ _:
     ./audio.nix
     ./bluetooth.nix
     ./hyprland.nix
+    ./gnome.nix
   ];
 }
--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,1 +1,15 @@
-{ }
+{ pkgs, ... }:
+
+{
+  # Enable the desktop environment GNOME
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Install a minimal set of apps to avoid the bloatware of GNOME
+  environment.systemPackages = with pkgs; [
+    gnome-terminal
+    gnome-control-center
+    nautilus
+  ];
+}
