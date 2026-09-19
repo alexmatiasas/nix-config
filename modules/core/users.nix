2b2d270489f5567b3584756a010aab4b5f7618d6
alexmatiasas <alejandromatiasastorga@gmail.com>
2026-09-19 04:19:15 -0600

    refactor(users): add default shell for user
    - set default shell to zsh

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -1,8 +1,9 @@
-_: {
+{ pkgs, ... }: {
   users.users.alexmatias = {
     isNormalUser = true;
     description = "Alex Matías";
     extraGroups = [ "wheel" ];
+    shell = pkgs.zsh;
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEvriL6yohuz3kNEPP2QQ/PungiDGgqIyTX4iUfLzl92 alejandromatiasas@gmail.com"
