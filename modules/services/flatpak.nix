7bb4356ac188e80437985870e568d007057f8c4b
alexmatiasas <alejandromatiasastorga@gmail.com>
2026-09-20 10:53:34 -0600

    refactor: avoid flatpak installations

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -3,23 +3,23 @@
 {
   services.flatpak.enable = true;
 
-  systemd.services.flatpak-install-apps = {
-    description = "Install essential Flatpak applications";
-    after = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeShellScript "flatpak-install-script" ''
-        ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-        # Instalación explícita: Sin variables, sin bucles, sin errores de interpolación
-        ${pkgs.flatpak}/bin/flatpak install -y flathub com.spotify.Client
-        ${pkgs.flatpak}/bin/flatpak install -y flathub com.discordapp.Discord
-        ${pkgs.flatpak}/bin/flatpak install -y flathub com.zoom.zoom
-        ${pkgs.flatpak}/bin/flatpak install -y flathub com.slack.Slack
-        ${pkgs.flatpak}/bin/flatpak install -y flathub com.dropbox.Dropbox
-        ${pkgs.flatpak}/bin/flatpak install -y flathub com.microsoft.Teams
-      '';
-    };
-  };
+  # systemd.services.flatpak-install-apps = {
+  #   description = "Install essential Flatpak applications";
+  #   after = [ "network-online.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = pkgs.writeShellScript "flatpak-install-script" ''
+  #       ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  #
+  #       # Instalación explícita: Sin variables, sin bucles, sin errores de interpolación
+  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.spotify.Client
+  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.discordapp.Discord
+  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.zoom.zoom
+  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.slack.Slack
+  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.dropbox.Dropbox
+  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.microsoft.Teams
+  #     '';
+  #   };
+  # };
 }
