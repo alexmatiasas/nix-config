df76d9ffaab0d35ca26f6c1c1d394a2816d2b285
alexmatiasas <alejandromatiasastorga@gmail.com>
2026-09-21 00:01:50 -0600

    feat: add development packages

--- profiles/server.nix
+++ profiles/server.nix
@@ -8,6 +8,7 @@ _:
     ../modules/programs/shell.nix
     ../modules/programs/desktop-apps.nix
     ../modules/services/ssh.nix
+    ../modules/development
     ../modules/services/tailscale.nix
     ../modules/services/backups.nix
     ../modules/services/flatpak.nix
