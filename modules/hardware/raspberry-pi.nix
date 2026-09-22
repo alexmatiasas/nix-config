3c79f7475a78073379f348c084b78001cc92f256
alexmatiasas <alejandromatiasastorga@gmail.com>
2026-09-21 19:50:05 -0600

    refactor(hardware/raspberry-pi): add zram swap configuration to mitigate SD card damage
    Introduce zram swap to utilize RAM for swapping, reducing the impact on SD card performance. This configuration enables zram swap with 50% of available RAM.

--- modules/hardware/raspberry-pi.nix
+++ modules/hardware/raspberry-pi.nix
@@ -1,1 +1,7 @@
 { }
+# In a raspberry-pi, the swap file damage SD memory, they need other approach
+# zram - swap compressed on RAM.
+# services.zramSwap = {
+#   enable = true;
+#   memoryPercent = 50;
+# };
