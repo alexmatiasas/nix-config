3fb65d752fcf9b36bc2cfd72698cbe608b9efd12
alexmatiasas <alejandromatiasastorga@gmail.com>
2026-09-25 00:14:10 -0600

    refactor: reformat and comment systemd services in `backups.nix` and `flatpak.nix`
    - Reorganized and commented systemd services for better readability.
    - Ensured consistency in service configuration across files.

--- modules/services/backups.nix
+++ modules/services/backups.nix
@@ -7,23 +7,23 @@
   ];
 
   # Configuration of a timer for systemd for automatic backups
-  # systemd.services.restic-backup = {
-  #   description = "Automatic Backup of Projects and Documents";
-  #   after = [ "network-online.target" ];
-  #   wantedBy = [ "timers.target" ];
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     # Executes the backup. NOTE: requires that the repository be initializated at home/
-  #     ExecStart = "${pkgs.restic}/bin/restic backup /home/alexmatias/projects /home/alexmatias/Documents";
-  #   };
-  # };
-  #
-  # systemd.timers.restic-backup = {
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     # Executes the backup every day at 3:00 AM
-  #     OnCalendar = "*-*-* 03:00:00";
-  #     Persistent = true; # If machine is off, then when restarts
-  #   };
-  # };
+  systemd.services.restic-backup = {
+    description = "Automatic Backup of Projects and Documents";
+    after = [ "network-online.target" ];
+    wantedBy = [ "timers.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      # Executes the backup. NOTE: requires that the repository be initializated at home/
+      ExecStart = "${pkgs.restic}/bin/restic backup /home/alexmatias/projects /home/alexmatias/Documents";
+    };
+  };
+
+  systemd.timers.restic-backup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      # Executes the backup every day at 3:00 AM
+      OnCalendar = "*-*-* 03:00:00";
+      Persistent = true; # If machine is off, then when restarts
+    };
+  };
 }
--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -3,32 +3,32 @@
 {
   services.flatpak.enable = true;
 
-  # systemd.services.flatpak-install-apps = {
-  #   description = "Install essential Flatpak applications";
-  #   after = [ "network-online.target" ];
-  #   wantedBy = [ "multi-user.target" ];
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     ExecStart = pkgs.writeShellScript "flatpak-install-script" ''
-  #       ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-  #
-  #       # Instalación explícita: Sin variables, sin bucles, sin errores de interpolación
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.spotify.Client
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.discordapp.Discord
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.zoom.zoom
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.slack.Slack
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.dropbox.Dropbox
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.microsoft.Teams
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.obsproject.Studio
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.usebottles.bottles
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub org.localsend.localsend_app
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub org.mozilla.thunderbird_esr
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub io.github.jeffshee.Hidamari
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub org.kde.okular
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.calibre_ebook.calibre
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub org.mozilla.firefox
-  #       ${pkgs.flatpak}/bin/flatpak install -y flathub com.getpostman.Postman
-  #     '';
-  #   };
-  # };
+  systemd.services.flatpak-install-apps = {
+    description = "Install essential Flatpak applications";
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = pkgs.writeShellScript "flatpak-install-script" ''
+        ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+        # Instalación explícita: Sin variables, sin bucles, sin errores de interpolación
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.spotify.Client
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.discordapp.Discord
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.zoom.zoom
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.slack.Slack
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.dropbox.Dropbox
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.microsoft.Teams
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.obsproject.Studio
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.usebottles.bottles
+        ${pkgs.flatpak}/bin/flatpak install -y flathub org.localsend.localsend_app
+        ${pkgs.flatpak}/bin/flatpak install -y flathub org.mozilla.thunderbird_esr
+        ${pkgs.flatpak}/bin/flatpak install -y flathub io.github.jeffshee.Hidamari
+        ${pkgs.flatpak}/bin/flatpak install -y flathub org.kde.okular
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.calibre_ebook.calibre
+        ${pkgs.flatpak}/bin/flatpak install -y flathub org.mozilla.firefox
+        ${pkgs.flatpak}/bin/flatpak install -y flathub com.getpostman.Postman
+      '';
+    };
+  };
 }
